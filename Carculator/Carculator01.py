583cb1a08fecdbeff0fe94b4778411524e9752ec
youngOt73r <41808398+youngOt73r@users.noreply.github.com>
2018-08-04 10:01:23 +0900

    Carculator by 영환
    동화 수정

--- Carculator/Carculator01.py
+++ Carculator/Carculator01.py
@@ -5,23 +5,18 @@ try:
     fir = int(cal.split(' ')[0])
     sec = int(cal.split(' ')[1])
 
-    f = cal.split(' ')[0]
-    s = cal.split(' ')[1]
     oper = input("연산자를 입력해주세요.\n")
+    c = Carmodule.Carc(fir, sec)
 
     if oper == '+':
-        c = Carmodule.Carc(fir, sec)
         print(str(c.plus())+" 입니다.")
     elif oper == '-':
-        c = Carmodule.Carc(fir, sec)
         print(str(c.minus())+" 입니다.")
     elif oper == '*':
-        c = Carmodule.Carc(fir, sec)
         print(str(c.mul())+" 입니다.")
     elif oper == '/':
-        c = Carmodule.Carc(fir, sec)
         print(str(c.div())+" 입니다.")
     else:
         print("nope")
-except Exception, e:
+except:
     print("check your input or Error : ", e)
