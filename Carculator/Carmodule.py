2291b82be76a10568a1ecb1cdb2b34f7af186cfe
Fhwang0926 <hdh0926@naver.com>
2018-08-04 09:17:07 +0900

    some fix code

--- Carculator/Carmodule.py
+++ Carculator/Carmodule.py
@@ -1,13 +1,27 @@
-print ("연산중입니다.\n")
+
+
 class Carc:
     def __init__(self, a, b):
         self.a = a
         self.b = b
+        # init when create this class 
+
     def plus(self):
+        print ("연산중입니다.\n")
         return self.a + self.b
+        # carcul
+
     def minus(self):
+        print ("연산중입니다.\n")
         return self.a - self.b
+        # carcul
+
     def mul(self):
+        print ("연산중입니다.\n")
         return self.a * self.b
+        # carcul
+
     def div(self):
+        print ("연산중입니다.\n")
         return self.a / self.b
+        # carcul
